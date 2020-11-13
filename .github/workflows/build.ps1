0f38da4f31a23e34d485cd5563933ace24349d11
kjclev <kjclev@gmail.com>
2020-11-13 11:00:40 +0900

    Updated build

--- .github/workflows/build.ps1
+++ .github/workflows/build.ps1
@@ -1,14 +1,17 @@
 $ErrorActionPreference = "Stop"
 
-$task = New-Item 'task.bat' -Force
-Add-Content $task 'call phpize 2>&1'
-Add-Content $task 'call configure --with-php-build=C:\build-cache\deps --enable-test --enable-debug-pack 2>&1'
-Add-Content $task 'nmake /nologo 2>&1'
-Add-Content $task 'exit %errorlevel%'
-& "C:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
-if (-not $?) {
-    throw "building failed with errorlevel $LastExitCode"
-}
+# $task = New-Item 'task.bat' -Force
+# Add-Content $task 'call phpize 2>&1'
+# Add-Content $task 'call configure --with-php-build=C:\build-cache\deps --enable-test --enable-debug-pack 2>&1'
+# Add-Content $task 'nmake /nologo 2>&1'
+# Add-Content $task 'exit %errorlevel%'
+# & "C:\build-cache\php-sdk-$env:BIN_SDK_VER\phpsdk-$env:VC-$env:ARCH.bat" -t $task
+# if (-not $?) {
+#     throw "building failed with errorlevel $LastExitCode"
+# }
+phpize
+configure --with-php-build=C:\build-cache\deps --enable-test --enable-debug-pack
+nmake /nologo
 
 $dname = ''
 if ($env:ARCH -eq 'x64') {
